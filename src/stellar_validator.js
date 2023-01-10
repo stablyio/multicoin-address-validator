0c3eafb8f4cb012a3779eedbb4684e90c6f39670
David Wright <davidwright@stably.io>
2023-01-10 14:52:36 -0800

    [WIP] Add support for Stellar Muxed

--- src/stellar_validator.js
+++ src/stellar_validator.js
@@ -1,23 +1,25 @@
-var baseX = require('base-x');
-var crc = require('crc');
-var cryptoUtils = require('./crypto/utils');
+var baseX = require("base-x");
+var crc = require("crc");
+var cryptoUtils = require("./crypto/utils");
 
-var ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+var ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
 
 var base32 = baseX(ALPHABET);
-var regexp = new RegExp('^[' + ALPHABET + ']{56}$');
-var ed25519PublicKeyVersionByte = (6 << 3);
+var regexp = new RegExp("^[" + ALPHABET + "]{56,69}$");
+var ed25519PublicKeyVersionByte = 6 << 3; // G
+const med25519PublicKeyVersionByte = 12 << 3; // M
 
 function swap16(number) {
-    var lower = number & 0xFF;
-    var upper = (number >> 8) & 0xFF;
+    var lower = number & 0xff;
+    var upper = (number >> 8) & 0xff;
     return (lower << 8) | upper;
 }
 
 module.exports = {
     isValidAddress: function (address) {
         if (regexp.test(address)) {
-            return this.verifyChecksum(address);
+            // return this.verifyChecksum(address);
+            return true;
         }
 
         return false;
@@ -26,13 +28,19 @@ module.exports = {
     verifyChecksum: function (address) {
         // based on https://github.com/stellar/js-stellar-base/blob/master/src/strkey.js#L126
         var bytes = base32.decode(address);
-        if (bytes[0] !== ed25519PublicKeyVersionByte) {
+        if (
+            bytes[0] !== ed25519PublicKeyVersionByte &&
+            bytes[0] !== med25519PublicKeyVersionByte
+        ) {
             return false;
         }
 
-        var computedChecksum = cryptoUtils.numberToHex(swap16(crc.crc16xmodem(bytes.slice(0, -2))), 4);
+        var computedChecksum = cryptoUtils.numberToHex(
+            swap16(crc.crc16xmodem(bytes.slice(0, -2))),
+            4
+        );
         var checksum = cryptoUtils.toHex(bytes.slice(-2));
 
-        return computedChecksum === checksum
-    }
+        return computedChecksum === checksum;
+    },
 };
