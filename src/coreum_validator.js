93bc4b20c3b3bd10a7ccb9dcdd71eeba88b1f85f
Hung <6251947+masterhung0112@users.noreply.github.com>
2023-04-27 20:37:24 +0700

    Add Coreum address validation #6

---
+++ src/coreum_validator.js
@@ -0,0 +1,25 @@
+var bech32 = require("./crypto/bech32");
+
+const validHrp = {
+    main: ["core"],
+    testnet: ["testcore", "devcore"],
+};
+
+function isValidAddress(address, currency, opts) {
+    const { networkType } = opts || {};
+    const decoded = bech32.decode(address, bech32.encodings.BECH32);
+
+    const validByNetworkType = validHrp[networkType] ?? validHrp.main;
+
+    if (decoded && validByNetworkType.includes(decoded.hrp.toLowerCase())) {
+        return true;
+    }
+
+    return false;
+}
+
+module.exports = {
+    isValidAddress: function (address, currency, networkType) {
+        return isValidAddress(address, currency, networkType);
+    },
+};
